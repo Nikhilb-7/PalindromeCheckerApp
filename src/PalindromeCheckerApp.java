cacaf3885007527a267a62f79b9216a717d8ad23
NIkhil <nikhilkumarbeera2k6@gmail.com>
2026-02-13 11:29:23 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,13 @@
+/*
+@author Nikhil
+@version 1.0
+Created class as PalindromeCheckerApp
+Printed Three Statements for  Application Entry and Welcome Message
+*/
+public class PalindromeCheckerApp {
+        public static void main (String[] args) {
+            System.out.println("Welcome to the Palindrome Checker Management System");
+            System.out.println("Version : 1.0");
+            System.out.println("system initialized successfully.");
+        }
+    }
